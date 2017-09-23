16185e130728a8b4f0b839d8cbad0a6765afb42e
muhammadmuneeb12345 <muneebnaeemsheikh99@gmail.com>
2017-09-23 04:36:01 -0700

    third

---
+++ Output.java
@@ -0,0 +1,14 @@
+public class Output {
+
+    int a=0;
+    int b=0;int c =0;
+    public Output(int a, int b){
+        this.a = a;
+        this.b = b;
+        c= a*b;
+    }
+    public void Print(){
+
+        System.out.println(c);
+    }
+}
